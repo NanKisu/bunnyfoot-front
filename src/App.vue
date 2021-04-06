cd404b6f4b1227366768cf93f6f8a4e5ad940612
nankisu0301 <nankisu0301@naver.com>
2021-04-06 20:36:56 +0900

    PC 화면 수정

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,22 @@ export default {
 <style>
 @import '../static/css/style.css';
 body {
+  background-color: #ccc;
+}
+
+@media screen and (min-width: 600px) {
+  #app {
+    min-height: 900px !important;
+    max-width: 600px !important;
+    margin: 0 auto;
+  }
+}
+#app {
+  position: relative;
+  top: 50vh;
+  margin-top: -450px;
+  max-width: 600px;
+  min-height: 900px;
   background-image: url("/static/images/pattern/leaves.png");
   /* height: 100%; */
   background-position: center;
