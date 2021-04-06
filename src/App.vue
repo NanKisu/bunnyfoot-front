7a07bb9e5e931b616e79442d70b0e7cd515f8d9b
nankisu0301 <nankisu0301@naver.com>
2021-04-06 20:39:49 +0900

    모바일 화면 수정

--- src/App.vue
+++ src/App.vue
@@ -18,17 +18,16 @@ body {
 
 @media screen and (min-width: 600px) {
   #app {
+    position: relative;
+    top: 50vh;
+    margin-top: -450px;
     min-height: 900px !important;
     max-width: 600px !important;
     margin: 0 auto;
   }
 }
 #app {
-  position: relative;
-  top: 50vh;
-  margin-top: -450px;
   max-width: 600px;
-  min-height: 900px;
   background-image: url("/static/images/pattern/leaves.png");
   /* height: 100%; */
   background-position: center;
