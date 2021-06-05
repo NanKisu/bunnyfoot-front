0cc73a0dbe40628f8e843f2ded47f8bc31f5aed3
nankisu0301 <nankisu0301@naver.com>
2021-06-06 01:10:42 +0900

    l

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/devtest'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
