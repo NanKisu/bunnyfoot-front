3cbd9f6ca464a700ebdc7e15be7719fa1d19d615
LeeKisu <nankisu0301@naver.com>
2021-06-05 15:05:35 +0900

    방문자 수정

--- src/components/WelComePage.vue
+++ src/components/WelComePage.vue
@@ -43,7 +43,7 @@ export default {
 
       axios.get(url)
         .then(rest => {
-          this.participant = rest.data - 420
+          this.participant = rest.data - 620
           this.pageViewVisible = true
         })
         .catch(() => {
